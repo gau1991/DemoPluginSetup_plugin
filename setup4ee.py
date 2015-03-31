902a80c496cfec941f56bb9e35244732a3847b90
rjdp <rjdp9736@gmail.com>
2015-03-31 16:06:00 +0530

    Add Setup, plugin,plugin config , so it integrates with Easyengine

---
+++ setup4ee.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+abspathconfig = '/root/.ee/plugins.d'
+abspathplugins = '/root/.ee/plugins'
+
+if not os.path.exists(abspathconfig):
+    os.makedirs(abspathconfig)
+
+if not os.path.exists(abspathplugins):
+    os.makedirs(abspathplugins)
+
+
+
+setup(name='Demo Plugin 4 ee',
+      version='0.1',
+      description='Demo Plugin',
+      url='http://github.com/rjdp',
+      author='rajdeep',
+      author_email='rajdeep.sharma@rtcamp.com',
+      license='MIT',
+      data_files=[(abspathconfig, ['testplugin/plugn4ee.conf']),
+                  (abspathplugins, ['testplugin/plugn4ee.py'])],
+      # packages=['cli'],
+      install_requires=[
+          'ee',
+      ],
+      
+      zip_safe=False)
